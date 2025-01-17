5613ed12b80a01d8ad0f76e7e3fe433401cfcc8e
DiTotto <luca.ditotto99@gmail.com>
2025-01-17 12:26:22 +0100

    added test for getCurrentDirectories method

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieImplTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieImplTest.java
@@ -1,13 +1,18 @@
 package org.apache.bookkeeper.bookie;
 
 import org.apache.bookkeeper.conf.ServerConfiguration;
+import org.apache.bookkeeper.helper.TmpDirs;
 import org.apache.bookkeeper.net.BookieId;
 import org.apache.bookkeeper.net.BookieSocketAddress;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Collection;
@@ -20,13 +25,13 @@ public class BookieImplTest {
 
     // Test per il metodo getBookieAddress
     @RunWith(Parameterized.class)
-    public static class BookieImplGetBookieAddressTest {
+    public static class BookieImplGetBookieAddressParameterizedTest {
 
         private final ServerConfiguration conf;
         private final boolean expectException;
 
         // Costruttore per inizializzare i parametri del test
-        public BookieImplGetBookieAddressTest(ServerConfiguration conf, boolean expectException) {
+        public BookieImplGetBookieAddressParameterizedTest(ServerConfiguration conf, boolean expectException) {
             this.conf = conf;
             this.expectException = expectException;
         }
@@ -97,13 +102,13 @@ public class BookieImplTest {
 
     // Test per il metodo getBookieId
     @RunWith(Parameterized.class)
-    public static class BookieImplGetBookieIdTest {
+    public static class BookieImplGetBookieIdParameterizedTest {
 
         private final ServerConfiguration conf;
         private final boolean expectException;
 
         // Costruttore per inizializzare i parametri del test
-        public BookieImplGetBookieIdTest(ServerConfiguration conf, boolean expectException) {
+        public BookieImplGetBookieIdParameterizedTest(ServerConfiguration conf, boolean expectException) {
             this.conf = conf;
             this.expectException = expectException;
         }
@@ -164,4 +169,98 @@ public class BookieImplTest {
             }
         }
     }
+
+    @RunWith(Parameterized.class)
+    public static class BookieImplGetCurrentDirectoriesParameterizedTest {
+
+        private static TmpDirs tmpDirs;
+        private static File existingDir;
+        private static File anotherExistingDir;
+        private static File validDir1;
+        private static File validDir2;
+        private static File inaccessibleDir;
+        private static File file;
+
+
+
+        private final File[] dirs;
+        private final boolean expectException;
+
+        public BookieImplGetCurrentDirectoriesParameterizedTest(File[] dirs, boolean expectException) {
+            this.dirs = dirs;
+            this.expectException = expectException;
+        }
+
+        @BeforeClass
+        public static void setUpClass() throws Exception {
+            tmpDirs = new TmpDirs();
+
+
+            /*existingDir = tmpDirs.createNew("existingDir", null);
+            anotherExistingDir = tmpDirs.createNew("anotherExistingDir", null);
+            */
+            validDir1 = tmpDirs.createNew("validDir1", null);
+            validDir2 = tmpDirs.createNew("validDir2", null);
+
+            // Creazione di una directory non accessibile
+            inaccessibleDir = tmpDirs.createNew("inaccessibleDir", null);
+            //inaccessibleDir.setReadable(false);
+            inaccessibleDir.setWritable(false);
+            // Creazione di un file
+            file = File.createTempFile("tempFile", ".tmp");
+            file.deleteOnExit();
+        }
+
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+                    /*// valid: array di dirs
+                    {new File[]{existingDir, anotherExistingDir}, false},
+                    // array vuoto
+                    {new File[]{}, false},
+                    // invalid: null
+                    {null, true},*/
+                    // valid: array di dirs
+                    {new File[]{validDir1, validDir2}, false},
+                    // Valid: Mixed valid and invalid (file + inaccessible)
+                    {new File[]{validDir1, file, inaccessibleDir}, false},
+                    // Valid: Empty input
+                    {new File[]{}, false},
+                    // Invalid: Null input
+                    {null, true},
+            });
+        }
+
+        @Test
+        public void testGetCurrentDirectories() {
+            try {
+                File[] result = BookieImpl.getCurrentDirectories(dirs);
+                if (expectException) {
+                    fail("Expected an exception, but none was thrown.");
+                }
+                assertNotNull("Result should not be null", result);
+
+            } catch (Exception e) {
+                if (!expectException) {
+                    fail("Unexpected exception thrown: " + e.getMessage());
+                }
+            }
+        }
+
+        @AfterClass
+        public static void tearDownClass() {
+            if (tmpDirs != null) {
+                try {
+                    tmpDirs.cleanup();
+                } catch (IOException e) {
+                    System.err.println("Failed to clean up temporary directories: " + e.getMessage());
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
 }
